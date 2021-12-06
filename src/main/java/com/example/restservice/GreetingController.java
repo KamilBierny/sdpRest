3536a8b3224a271bcb47dd298fbf2e6da8d84313
kamil.biernat <kamil.biernat@globallogic.com>
2021-12-06 09:11:51 +0100

    SDP rest service

---
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -0,0 +1,25 @@
+package com.example.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    /*
+     * TODO: w requescie powinny byc 2 stringi, ktore nalezy dodac do siebie, zwrocic do klienta jako jeden, oraz godzine jako
+     * dodatkowe pole. kamil.biernat@globallogic.com do 21.12.21 wysle na maile prezentacje i kod.
+     */
+
+}
